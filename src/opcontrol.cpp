fe20e2348f2542ea9176620e0ddb90b046b59b05
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-02-05 21:30:07 -0500

    Added more comments to opcontrol.cpp

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -7,6 +7,7 @@ extern ChassisControllerIntegrated chassis;
 Controller controller (ControllerId::master);
 
 void intakeControl (void * param) {
+  //loop to control intake motor
   while (true) {
     if (controller.getDigital(ControllerDigital::R1)) intake.moveVoltage(12000);
     else if (controller.getDigital(ControllerDigital::R2)) intake.moveVoltage(-12000);
@@ -17,6 +18,7 @@ void intakeControl (void * param) {
 
 void opcontrol() {
   pros::Task intakeTask (intakeControl);
+  //loop to control the drive train
   while (true) {
     chassis.tank(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::rightY), 0.2);
     pros::Task::delay(10);
