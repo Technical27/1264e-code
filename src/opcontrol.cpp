761eb2bd81e80d3d1edcf059cdddd3ab2a1cf91d
Technical27 <38222826+Technical27@users.noreply.github.com>
2019-02-07 16:01:23 -0500

    Added the base for lift control and 2 controller support

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -4,23 +4,30 @@ extern MotorGroup leftSide;
 extern MotorGroup rightSide;
 extern Motor intake;
 extern ChassisControllerIntegrated chassis;
-Controller controller (ControllerId::master);
+Controller mainController (ControllerId::master);
+Controller auxController (ControllerId::partner);
 
 void intakeControl (void * param) {
   //loop to control intake motor
   while (true) {
-    if (controller.getDigital(ControllerDigital::R1)) intake.moveVoltage(12000);
-    else if (controller.getDigital(ControllerDigital::R2)) intake.moveVoltage(-12000);
+    if (mainController.getDigital(ControllerDigital::R1)) intake.moveVoltage(12000);
+    else if (mainController.getDigital(ControllerDigital::R2)) intake.moveVoltage(-12000);
     else intake.moveVoltage(0);
     pros::Task::delay(10);
   }
 }
 
+void liftControl (void * param) {
+
+}
+
 void opcontrol() {
+  //tasks to control other functions
   pros::Task intakeTask (intakeControl);
+  pros::Task liftTask (liftControl);
   //loop to control the drive train
   while (true) {
-    chassis.tank(controller.getAnalog(ControllerAnalog::leftY), controller.getAnalog(ControllerAnalog::rightY), 0.2);
+    chassis.tank(mainController.getAnalog(ControllerAnalog::leftY), mainController.getAnalog(ControllerAnalog::rightY), 0.2);
     pros::Task::delay(10);
   }
 }
