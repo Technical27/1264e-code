97830431ada096b510e712e7ad54124b2000a98a
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-02-08 21:35:29 -0500

    bug fixes and a reverse drive mode

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -7,6 +7,7 @@ extern Motor liftTilt;
 extern ChassisControllerIntegrated chassis;
 Controller mainController (ControllerId::master);
 Controller auxController (ControllerId::partner);
+bool reversed = false;
 
 void intakeControl (void * param) {
   //loop to control intake motor
@@ -24,14 +25,23 @@ void liftControl (void * param) {
     else if (auxController.getDigital(ControllerDigital::L2)) lift.moveVoltage(-12000);
     else lift.moveVoltage(0);
 
-    if (auxController.getDigital(ControllerDigital::R1) && liftTilt.getTargetPosition() == 0) liftTilt.moveRelative(1350, 100);
-    else if (auxController.getDigital(ControllerDigital::R2) && liftTilt.getTargetPosition() == 0) liftTilt.moveRelative(-1350, 100);
+    if (auxController.getDigital(ControllerDigital::R1)) liftTilt.moveVoltage(6000);
+    else if (auxController.getDigital(ControllerDigital::R2)) liftTilt.moveVoltage(-6000);
+    else liftTilt.moveVoltage(0);
   }
 }
 
 void launcherControl (void * param) {
   while (true) {
-    if (mainController.getDigital(ControllerDigital::L2) && launcher.getTargetPosition() == 0) launcher.moveRelative(1800, 100);
+    if (mainController.getDigital(ControllerDigital::L2)) launcher.moveRelative(1800, 100);
+  }
+}
+
+void driveControl (void * param) {
+  while (true) {
+    if (!reversed) chassis.tank(mainController.getAnalog(ControllerAnalog::leftY), mainController.getAnalog(ControllerAnalog::rightY), 0.2);
+    else if (reversed) chassis.tank(-mainController.getAnalog(ControllerAnalog::rightY), -mainController.getAnalog(ControllerAnalog::leftY), 0.2);
+    pros::Task::delay(10);
   }
 }
 
@@ -40,9 +50,10 @@ void opcontrol() {
   pros::Task intakeTask (intakeControl);
   pros::Task liftTask (liftControl);
   pros::Task launcherTask (launcherControl);
+  pros::Task driveTask (driveControl);
   //loop to control the drive train
   while (true) {
-    chassis.tank(mainController.getAnalog(ControllerAnalog::leftY), mainController.getAnalog(ControllerAnalog::rightY), 0.2);
-    pros::Task::delay(10);
+    if (mainController.getDigital(ControllerDigital::L1)) reversed = !reversed;
+    pros::Task::delay(50);
   }
 }
