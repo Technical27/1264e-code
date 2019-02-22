650132460144aa14bef5641e7b78a9c90eef97d1
Technical27 <38222826+Technical27@users.noreply.github.com>
2019-02-22 16:24:51 -0500

    new autonomous

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -8,12 +8,32 @@ void autonomous() {
   init();
   intake.moveVoltage(-12000);
   pros::Task::delay(5000);
+
+  intake.moveVoltage(0);
+  launcher.moveRelative(1800, 100);
+  pros::Task::delay(1000);
+
+  chassis.moveDistance(1500);
+  chassis.moveDistance(-1400);
+  chassis.turnAngle(90_deg);
+  chassis.moveDistance(-350);
+
+  intake.moveVoltage(-12000);
+  chassis.setMaxVoltage(3000);
+  chassis.moveDistance(1600);
+  intake.moveVoltage(0);
+
+  chassis.moveDistance(-400);
+  intake.moveVoltage(12000);
+  chassis.moveDistance(400);
+  intake.moveVoltage(0);
+
+  chassis.turnAngle(-90_deg);
+  chassis.setMaxVoltage(12000);
+  intake.moveVoltage(-12000);
+  pros::Task::delay(2000);
   intake.moveVoltage(0);
   launcher.moveRelative(1800, 100);
   pros::Task::delay(1000);
   chassis.moveDistance(1500);
-  chassis.moveDistance(-1990);
-  chassis.turnAngle(90_deg);
-  chassis.moveDistance(-350);
-  chassis.moveDistance(3900);
 }
