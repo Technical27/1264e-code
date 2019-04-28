2d1b4e6954f33cb63e9a86718a525f562a08a53c
Technical27 <38222826+Technical27@users.noreply.github.com>
2019-04-28 08:20:48 -0400

    fixed style

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,7 +1,7 @@
 #include "include.hpp"
 Controller mainController (ControllerId::master);
 
-void driveControl (void * param) {
+void driveControl (void* param) {
   while (true) {
     chassis.tank(mainController.getAnalog(ControllerAnalog::leftY), mainController.getAnalog(ControllerAnalog::rightY), 0.2);
     pros::Task::delay(10);
