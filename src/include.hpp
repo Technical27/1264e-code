0ef31e2d3f7d46c958b079384e5a3e606d7deccf
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-05-04 21:37:42 -0400

    added more variables to extern

--- src/include.hpp
+++ src/include.hpp
@@ -4,3 +4,13 @@ extern Motor frontLeft;
 extern Motor frontRight;
 extern Motor backLeft;
 extern Motor backRight;
+
+extern MotorGroup left;
+extern MotorGroup right;
+
+extern Controller mainController;
+
+extern pros::Mutex motorMutex;
+
+void driveControl (void* param);
+void strafeControl (void* param);
