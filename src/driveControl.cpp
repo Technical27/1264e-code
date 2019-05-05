d452269aed7a4a7b8fdb30630ea86559df967feb
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-05-04 21:38:39 -0400

    improved strafing and both functions are now seperate

---
+++ src/driveControl.cpp
@@ -0,0 +1,18 @@
+#include "include.hpp"
+
+void driveControl (void* param) {
+  while (true) {
+    pros::Task::delay(25);
+    if (!motorMutex.take(0)) continue;
+    motorMutex.take(5);
+    left.moveVoltage(mainController.getAnalog(ControllerAnalog::leftY) * 12000);
+    right.moveVoltage(mainController.getAnalog(ControllerAnalog::rightY) * 12000);
+    motorMutex.give();
+  }
+}
+
+void opcontrol () {
+  pros::Task driveTask (driveControl);
+  pros::Task strafeTask (strafeControl);
+  while (true) pros::Task::delay(10);
+}
