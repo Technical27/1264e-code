ca39e7192dcc53f004d55fd8ffcf7e2a156af69e
Technical27 <38222826+Technical27@users.noreply.github.com>
2019-05-12 12:42:24 -0400

    added more comments

--- src/include.hpp
+++ src/include.hpp
@@ -1,17 +1,26 @@
 #include "main.h"
 
-// variable and function definitions
+// Individual motors
 extern Motor frontLeft;
 extern Motor frontRight;
 extern Motor backLeft;
 extern Motor backRight;
 
+// Left and right motor groups
 extern MotorGroup left;
 extern MotorGroup right;
 
+// menu buttons
+extern ADIButton menuLeft;
+extern ADIButton menuRight;
+
+// The main controller
 extern Controller mainController;
 
+// Mutex for motor access
 extern pros::Mutex motorMutex;
 
+// Task functions
 void driveControl (void* param);
 void strafeControl (void* param);
+void screenControl (void* param);
